b968f3b0c4062b29b2df4be3fa348f40d0b1a32b
knikh <knikh@DESKTOP-9C778NE>
2024-02-13 08:31:07 +0530

    assignment

--- Automation/src/Assignment/A33_Dream11_EnterNumber_SS.java
+++ Automation/src/Assignment/A33_Dream11_EnterNumber_SS.java
@@ -17,13 +17,8 @@ public class A33_Dream11_EnterNumber_SS {
 		d.get("https://www.dream11.com/");
 		Thread.sleep(2000);
 	
-		WebElement scrollTill = d.findElement(By.xpath("//div[@class='banner_title_new']")) ; 
-		JavascriptExecutor js = (JavascriptExecutor)d;
-		js.executeScript("arguments[0].scrollIntoView(true)", scrollTill);
-		Thread.sleep(1000);
-		WebElement phoneNo = d.findElement(By.xpath("//input[@placeholder='Enter Mobile Number']"));
-		phoneNo.click();
-		phoneNo.sendKeys("3456789345");
+	    d.switchTo().frame(0);
+	    d.findElement(By.xpath("//input[@placeholder='Enter Mobile Number']")).sendKeys("3456789345");
 		Thread.sleep(1000);
 		d.findElement(By.xpath("//a[text()='GET APP LINK']")).click();
 		
--- Automation/src/Assignment/A34_Google_AllFrames.java
+++ Automation/src/Assignment/A34_Google_AllFrames.java
@@ -14,10 +14,21 @@ public class A34_Google_AllFrames {
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		d.get("https://www.google.co.in/");
 		Thread.sleep(2000);
-		d.findElement(By.xpath("//button[text()='Stay signed out']")).click();
+		
+		//click on google apps
+		d.findElement(By.xpath("//a[@aria-label='Google apps']")).click();
+			
+		//switch to frame by using index
+		//d.switchTo().frame(1);
+				
+		//switch to frame by using name attribute value
 		d.switchTo().frame("app");
-		d.findElement(By.xpath("//a[@href='https://www.google.co.in/intl/en/about/products']")).click();
+				
+		//click on google maps
 		d.findElement(By.xpath("//span[text()='Maps']")).click();
+		
+		Thread.sleep(2000);
+		d.quit();
 	}
 
 }
